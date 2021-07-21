0919c27388725b7b778978b2b72ecc52f52cc179
renanll <renan_ll@hotmail.com>
2021-07-20 22:47:04 -0300

    add SeachCEP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import SafeEnvironment from './ui/styles/components/feedback/SafeEnvironment/SafeEnvironment';
 import PageTitle from './ui/styles/components/data-display/PageTitle/PageTitle';
 import UserInformation from './ui/styles/components/data-display/UserInformation/UserInformation';
-import { UserInformationContainer } from './ui/styles/components/data-display/UserInformation/UserInformation.style';
+import TextFieldMask from './ui/styles/components/inputs/TextFieldMask/TextFielMask'
+import { Button, Typography } from '@material-ui/core';
+import { typography } from '@material-ui/system';
 
 export default function Home() {
   return (
@@ -13,6 +15,20 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
+
+      <TextFieldMask
+        mask = {'99.999-999'}
+        label={'Digite seu CEP'}
+        fullWidth
+        variant = {'outlined'}
+      />
+      <Typography color={'error'} >CEP inválido</Typography>
+      <Button
+        variant = {'contained'}
+        color = {'secondary'}
+        sx = {{width:'220px'}}
+      >Buscar</Button>
+
       <UserInformation
         name = {'Renan Lemes'}
         picture = {'https://github.com/renan-lemes.png'}
